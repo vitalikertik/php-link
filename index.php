b82a573db85b9628db635813dcee1868e556e7c9
Vitaly Kertik <Vital.kertik.70@gmail.com>
2021-11-27 14:03:49 +0200

    if+html.variant1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php
+    $isAdmin= true;
+
+    if($isAdmin){git intl_is_failure
+     ?> 
+<h2>Code for admin</h2>
+     <?php } else{ ?> 
+   
+        <h2>Code for guest</h2>
+
+        <?php }  ?>
+</body>
+</html>
